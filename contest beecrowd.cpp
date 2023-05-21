51ed4c075c3f7eba101b0a9d97b3a2ad93dfebd1
Mustafa817 <133186044+Mustafa817@users.noreply.github.com>
2023-05-21 14:23:17 +0600

    Add files via upload

---
+++ contest beecrowd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double  R,A,PI=3.14159;
+    cin>>R;
+    cout<<showpoint;
+    cout<<fixed;
+    cout<<setprecision(4);
+
+    A = PI*R*R;
+    cout << "A="<<A<<endl;
+}
